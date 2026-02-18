3b855e73e2e9f43c9a9d410dbe7f720c14baa6bb
Wzuhqa <guestguest1251@gmail.com>
2026-02-19 00:27:54 +0530

    fix: register GSAP TextPlugin and remove font preload

--- prisma-murder-mystery/src/components/cinematic/CinematicIntro/scenes/DarknessScene.jsx
+++ prisma-murder-mystery/src/components/cinematic/CinematicIntro/scenes/DarknessScene.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
+import { TextPlugin } from 'gsap/TextPlugin'
 import styles from '../CinematicIntro.module.css'
+
+gsap.registerPlugin(TextPlugin)
 
 /**
  * Scene 1: Darkness
